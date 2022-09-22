f1d987f11e9f33f58813a3bad08de520348b5372
Emanuela Gonzales <emanuela.gonzales@student.univaq.it>
2022-09-22 19:00:24 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './styles/main.scss';
+
 import Layout from "./Layout";
 import Home from "./pages/Home";
 import Destinations from "./pages/Destinations";
